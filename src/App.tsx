683e5cdd5e3fe5dd35c786c3dc1373a2c378c87f
soeun-727 <soeun2215562@sookmyung.ac.kr>
2025-12-27 22:54:04 +0900

    chore: 컴포넌트 추가 및 패키지 재설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Initial from "./components/auth/Initial";
+
 export default function App() {
   return (
     <>
       <div className="bg-red-500 text-white p-5 text-2xl">
         <h1>Tailwind Test</h1>
       </div>
+      <Initial />
     </>
   );
 }
---
+++ src/components/auth/Initial.tsx
@@ -0,0 +1,7 @@
+export default function Initial() {
+  return (
+    <>
+      <h1>Initial Component</h1>
+    </>
+  );
+}
