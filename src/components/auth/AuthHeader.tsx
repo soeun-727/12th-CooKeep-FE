79b9cae3e6a1be222ee8ecffa25ffcbae3884f1b
ji070 <jiye2705@gmail.com>
2026-01-04 19:19:56 +0900

    feat: 로그인/회원가입용 AuthHeader 추가

---
+++ src/components/auth/AuthHeader.tsx
@@ -0,0 +1,17 @@
+// src/components/auth/AuthHeader.tsx
+import Logo from "../../assets/Logo_black.svg";
+
+const AuthHeader = () => {
+  return (
+    <header className="flex items-start justify-between px-4 py-3">
+      <img src={Logo} alt="CooKeep logo" className="w-[70px] h-[20px]" />
+
+      <p className="flex items-center gap-[6px] typo-label">
+        <span className="text-[#7D7D7D]">맛있는 습관이 이어지는 곳,</span>
+        <span className="text-[var(--color-green-deep)]">쿠킵</span>
+      </p>
+    </header>
+  );
+};
+
+export default AuthHeader;
