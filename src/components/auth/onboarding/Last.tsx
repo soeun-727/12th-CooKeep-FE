4db4c770c32b6c99cb55d9859c1ff70241526729
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-01-08 21:30:45 +0900

    fix: 오류 수정

--- src/components/auth/onboarding/Last.tsx
+++ src/components/auth/onboarding/Last.tsx
@@ -9,18 +9,20 @@ interface LastProps {
 export default function Last({ onStart }: LastProps) {
   return (
     <>
-      <div className="flex flex-col items-center">
-        <h1 className="typo-h1 mt-[166px]">답변 완료!</h1>
-        <h1 className="typo-h1">쿠킵하러 떠나볼까요</h1>
-        <div className="typo-body1 text-[var(--color-green-deep)] mt-[10px]">
-          답변은 앞으로의 AI 레시피 추천에 반영돼요
+      <div className="min-h-screen flex flex-col items-center">
+        <h1 className="typo-h1 mt-[112px]">답변 완료!</h1>
+        <h1 className="typo-h1">목표는 매주 자유롭게</h1>
+        <h1 className="typo-h1">변경할 수 있어요</h1>
+        <div className="typo-body1 text-[var(--color-green-deep)] font-bold mt-[10px]">
+          입력한 답변을 바탕으로 AI 레시피를 추천해드려요
         </div>
-        <img src={temp} className="w-[184px] mt-[122px]" />
-      </div>
-      <div className="fixed bottom-0 mb-[34px] left-1/2 -translate-x-1/2">
-        <Button size="L" onClick={onStart}>
-          시작하기
-        </Button>
+        <img src={temp} className="w-[184px] mt-[86px]" />
+
+        <div className="mt-[201px]">
+          <Button size="L" onClick={onStart}>
+            시작하기
+          </Button>
+        </div>
       </div>
     </>
   );
--- src/components/auth/onboarding/Notification.tsx
+++ src/components/auth/onboarding/Notification.tsx
@@ -13,30 +13,33 @@ export default function Notification() {
 
   return (
     <>
-      <div className="flex flex-col items-center w-[361px] mx-auto">
-        <h1 className="typo-h1 mt-[166px] text-center">
+      <div className="min-h-screen flex flex-col w-[361px] mx-auto">
+        <h1 className="typo-h1 mt-[107px] text-left">
           쿠킵 루틴, 알림으로 받아보시겠어요?
         </h1>
-        {/* 설명 문구는 h1보다는 조금 작은 폰트나 body급이 가독성이 좋습니다. */}
-        <p className="typo-body2 text-gray-500 mt-4 text-center break-keep">
-          유통기한 임박, 주간 목표, 물 주기처럼 까먹지 않게 필요한 순간에만
-          도와드릴게요. 언제든지 설정에서 변경할 수 있어요.
+        <p className="typo-body2 text-gray-500 mt-1 text-left break-keep">
+          유통기한 임박, 주간 목표, 물 주기처럼 까먹지 않게
+          <br />
+          필요한 순간에만 도와드릴게요.
+          <br />
+          언제든지 설정에서 변경할 수 있어요.
         </p>
 
-        <img src={temp} className="w-[184px] mt-[122px]" />
-      </div>
-
-      <div className="fixed bottom-0 mb-[34px] left-1/2 -translate-x-1/2 flex flex-col gap-2">
-        <Button
-          size="S"
-          className="!bg-[var(--color-green-deep)]"
-          onClick={handleFinish}
-        >
-          알림을 켤게요
-        </Button>
-        <Button size="S" className="!bg-gray-300" onClick={handleFinish}>
-          괜찮아요
-        </Button>
+        <div className="flex flex-col items-center justify-center">
+          <img src={temp} className="w-[184px] mt-[122px]" />
+          <div className="flex flex-col gap-2 mt-50">
+            <Button
+              size="S"
+              className="!bg-[var(--color-green-deep)]"
+              onClick={handleFinish}
+            >
+              알림을 켤게요
+            </Button>
+            <Button size="S" className="!bg-gray-300" onClick={handleFinish}>
+              괜찮아요
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   );
