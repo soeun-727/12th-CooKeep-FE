df122b2bce815f960fd4506e1ae6eb47203fbdae
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-01-09 23:47:16 +0900

    feat: /login 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 
 import InitialPage from "./pages/auth/InitialPage";
-import OnboardingPage from "./pages/login/OnboardingPage";
+import OnboardingPage from "./pages/auth/OnboardingPage";
 import LoginPage from "./pages/auth/LoginPage";
 
 export default function App() {
   return (
-    // <AppLayout>
-    //   <Routes>
-    //     <Route path="/" element={<InitialPage />} />
-    //   </Routes>
-    // </AppLayout>
     <AppLayout>
       <Routes>
-        {/* <Route path="/" element={<InitialPage />} /> */}
-        {/* <Route path="/" element={<OnboardingPage />} /> */}
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<InitialPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
       </Routes>
     </AppLayout>
   );
--- src/components/auth/Initial.tsx
+++ src/components/auth/Initial.tsx
@@ -1,17 +1,19 @@
 import icon from "../../assets/initial/icon.svg";
 import logo from "../../assets/initial/Logo.png";
 import Button from "../ui/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function Initial() {
+  const navigate = useNavigate();
   return (
-    <div className="relative min-h-screen bg-[var(--color-green-deep)]">
+    <div className="min-h-screen flex flex-col bg-[var(--color-green-deep)]">
       {/* 상단 텍스트 영역 */}
       <img
         src={icon}
         alt="Frame 아이콘"
-        className="w-[18.5px] absolute top-[140px] left-[165px]"
+        className="w-[18.5px] mt-21 ml-[169px]"
       />
-      <div className="absolute top-[155px] left-[32px]">
+      <div className="ml-8">
         <h1 className="typo-h1 flex gap-1">
           <span className="text-neutral-800">재료</span>
           <span className="text-white">관리부터,</span>
@@ -24,36 +26,26 @@ export default function Initial() {
       </div>
 
       {/* 로고 */}
-      <div className="absolute top-[220px] left-[32px]">
+      <div className="mt-3 ml-8">
         <img src={logo} alt="로고" className="w-[255px]" />
       </div>
 
-      {/* 버튼 영역 */}
-      <div
-        className="
-          absolute
-          top-[678px]
-          left-1/2
-          -translate-x-1/2
-        "
-      >
-        <Button size="L">시작하기</Button>
-      </div>
+      <div className="flex flex-col items-center justify-center gap-4">
+        {/* 버튼 영역 */}
+        <div className="mt-[385px]">
+          <Button size="L">시작하기</Button>
+        </div>
 
-      {/* 로그인 영역 */}
-      <div
-        className="
-          absolute
-          top-[750px]
-          left-1/2
-          -translate-x-1/2
-          flex
-          items-center
-          gap-[16px]
-        "
-      >
-        <span className="typo-caption text-white/80">이미 계정이 있나요?</span>
-        <button className="typo-caption text-white">로그인</button>
+        {/* 로그인 영역 */}
+        <div className="flex gap-4">
+          <span className="typo-caption text-white">이미 계정이 있나요?</span>
+          <button
+            onClick={() => navigate("/login")}
+            className="typo-caption text-white"
+          >
+            로그인
+          </button>
+        </div>
       </div>
     </div>
   );
