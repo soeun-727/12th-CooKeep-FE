a802ece22e8b69606e041945e41d866a2e48b811
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-01-18 16:38:00 +0900

    fix: 메인헤더 수정

--- src/components/fixed/MainHeader.tsx
+++ src/components/fixed/MainHeader.tsx
@@ -21,31 +21,32 @@ const MainHeader = () => {
     <header
       className="
         absolute top-0 z-50
-        w-[400px] max-w-[450px]
+        w-full max-w-[450px]
         h-[102px] 
         bg-[#FAFAFA]
         px-4
       "
     >
-      <div className="relative w-full h-full flex items-end pb-4">
-        <div className="absolute left-1/2 -translate-x-1/2 bottom-5">
-          <img
-            src={Logo}
-            alt="CooKeep logo"
-            className="w-20 object-contain pb-1"
-          />
-        </div>
-        {isListView && (
-          <div className="absolute left-0 bottom-4 flex items-center h-10">
+      <div className="relative justify-between w-full h-full flex items-end pb-4">
+        <div className="w-10 h-10 flex items-center">
+          {isListView && (
             <button
               className="p-1 active:opacity-50 transition-opacity"
               onClick={handleBack}
             >
               <img src={backIcon} alt="back" className="w-9" />
             </button>
-          </div>
-        )}
-        <div className="w-full flex justify-end items-center h-10">
+          )}
+        </div>
+        <div className="flex-1 flex justify-center">
+          <img
+            src={Logo}
+            alt="CooKeep logo"
+            className="w-20 object-contain pb-1"
+          />
+        </div>
+
+        <div className="w-10 h-10 flex items-center justify-end">
           <button className="p-1" onClick={handleSettings}>
             <img src={settings} alt="settings" className="w-9" />
           </button>
