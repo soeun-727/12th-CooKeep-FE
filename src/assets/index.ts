d9ac7d37cfa4d6aa117b6d24396776d3a95a392d
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-01-25 15:04:33 +0900

    chore: 이미지 파일 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+export { default as cookingChar } from "./character/cook_char.svg";
+export { default as seeds } from "./cookeeps/seeds.svg";
+export { default as seedling } from "./cookeeps/seedling.svg";
+export { default as plant } from "./cookeeps/plant.svg";
+export { default as tree } from "./cookeeps/tree.svg";
+export { default as arrow } from "./cookeeps/arrow.svg";
+export { default as earth } from "./cookeeps/earth.svg";
