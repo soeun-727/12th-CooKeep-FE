697b47bc371de045606755aa5b9bfd8b37dbee59
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-02-04 13:54:41 +0900

    feat: 달력 날짜 선택 시 확인 가능

--- src/pages/myCookeep/MyCookeepPage.tsx
+++ src/pages/myCookeep/MyCookeepPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import MyCookeepTabBar from "../../components/myCookeep/fixed/MyCookeepTabBar";
 import Profile from "../../components/myCookeep/fixed/Profile";
@@ -6,36 +6,48 @@ import Statistics from "../../components/myCookeep/contents/Statistics";
 import Calendar from "../../components/myCookeep/contents/Calendar";
 import RecordEntry from "../../components/myCookeep/record/RecordEntry";
 import AddMoreModal from "../../components/myCookeep/record/AddMoreModal";
-import tempImage from "../../assets/temporary-image.png";
 import { hasTodayRecord } from "../../utils/record";
+import type { CookeepRecord } from "../../types/record";
+import { useCookeepRecordStore } from "../../stores/useCookeepRecordStore";
+import RecordCard from "../../components/myCookeep/record/RecordCard";
 
 type TabType = "record" | "calendar" | "statistics";
-
-const recordData = {
-  "2026.01.19": tempImage,
-  "2026.01.20": tempImage,
-  "2026.01.21": tempImage,
-  "2026.01.25": tempImage,
-};
 
 export default function MyCookeepPage() {
   const location = useLocation();
   const navigate = useNavigate();
-
+  const records = useCookeepRecordStore((state) => state.records);
   const [activeTab, setActiveTab] = useState<TabType>("record");
   const [dismissed, setDismissed] = useState(false);
-
-  // ✅ 핵심: 초기값에서만 판단
+  const [selectedRecords, setSelectedRecords] = useState<CookeepRecord[]>([]);
   const [enteredByBottomTab, setEnteredByBottomTab] = useState(
     location.state?.fromTab === true,
   );
+  const calendarRecordMap = useMemo(() => {
+    return records.reduce(
+      (acc, rec) => {
+        const dateKey = rec.createdAt.replaceAll("-", ".");
+        acc[dateKey] = rec.images[0]?.url;
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
+  }, [records]);
 
-  /** 내부(MyCookeep) 탭 이동 */
   const handleTabChange = (tab: string) => {
     if (tab === "record" || tab === "calendar" || tab === "statistics") {
+      setSelectedRecords([]);
       setActiveTab(tab);
       setDismissed(false);
-      setEnteredByBottomTab(false); // 내부 이동이면 절대 모달 X
+      setEnteredByBottomTab(false);
+    }
+  };
+  const handleDateClick = (dateStr: string) => {
+    const founds = records.filter(
+      (r) => r.createdAt.replaceAll("-", ".") === dateStr,
+    );
+    if (founds.length > 0) {
+      setSelectedRecords(founds);
     }
   };
 
@@ -48,11 +60,17 @@ export default function MyCookeepPage() {
   const renderContent = () => {
     switch (activeTab) {
       case "calendar":
+        if (selectedRecords.length > 0) {
+          return (
+            <div className="flex flex-col items-center gap-6 px-4 animate-in fade-in duration-300">
+              {selectedRecords.map((record) => (
+                <RecordCard key={record.id} record={record} />
+              ))}
+            </div>
+          );
+        }
         return (
-          <Calendar
-            records={recordData}
-            onDateClick={(date) => console.log(date)}
-          />
+          <Calendar records={calendarRecordMap} onDateClick={handleDateClick} />
         );
       case "statistics":
         return <Statistics />;
