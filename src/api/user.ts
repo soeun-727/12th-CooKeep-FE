f434760f37883222d1fc3aea848ae45e19ff5d2c
ji070 <jiye2705@gmail.com>
2026-02-06 22:39:12 +0900

    feat: 닉네임 변경 api 연결하기

--- src/api/user.ts
+++ src/api/user.ts
@@ -15,3 +15,11 @@ export const saveOnboardingInfo = (data: OnboardingData) => {
 export const updatePushConsent = (marketingConsent: boolean) => {
   return api.patch("/api/users/me/onboarding/push", { marketingConsent });
 };
+
+export const updateNickname = async (nickname: string) => {
+  const res = await api.patch("/api/users/me/nickname", {
+    nickname,
+  });
+
+  return res.data;
+};
