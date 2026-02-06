cc17476e566db0b23bc194f4c71c3a207c134b85
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-02-06 21:21:52 +0900

    fix: 글자제한 설정

--- src/components/fridge/addItems/components/edit/MemoEditor.tsx
+++ src/components/fridge/addItems/components/edit/MemoEditor.tsx
@@ -7,15 +7,24 @@ interface MemoEditorProps {
 }
 
 export default function MemoEditor({ value, onSave }: MemoEditorProps) {
+  const MAX_LENGTH = 100;
   const [text, setText] = useState(value || "");
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const val = e.target.value;
+    if (val.length <= MAX_LENGTH) {
+      setText(val);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center mt-[18px] mb-16 pb-[18px]">
       <textarea
         autoFocus
         value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="메모를 입력해주세요"
+        onChange={handleChange}
+        maxLength={MAX_LENGTH}
+        placeholder="메모를 입력해주세요(최대 100글자)"
         className="w-[361px] h-49 p-3 rounded-[10px] border border-[#D1D1D1] text-center outline-none resize-none typo-body"
       />
 
