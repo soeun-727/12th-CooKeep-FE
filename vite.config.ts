41865590d503749cd40be2fc8275defe35d0fbf0
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-02-10 22:10:23 +0900

    fix: pwa 관련 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,46 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "appIcon.png", "appIcon.svg"], // 캐싱할 정적 자산
+      manifest: {
+        name: "CooKeep",
+        short_name: "CooKeep",
+        description: "지금 있는 재료로 즐거운 요리 경험, CooKeep",
+        theme_color: "#FAFAFA",
+        background_color: "#FAFAFA",
+        display: "standalone",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
