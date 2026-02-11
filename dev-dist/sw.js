1a229567598e93babe15bb39834ab62849dc00c6
ji070 <jiye2705@gmail.com>
2026-02-12 03:54:32 +0900

    feat: dev-dist 가 수정되었다고 해서 올립니다

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.b0bh5n5ojtg"
+    "revision": "0.ff2j626gako"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
