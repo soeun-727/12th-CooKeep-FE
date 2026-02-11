94aca0dc21395178d5dadf9be0adb838647ecbfc
ji070 <jiye2705@gmail.com>
2026-02-12 04:09:23 +0900

    feat: 로딩페이지 로딩 아이콘 바꿔봄

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.dnbaj6ldvgo"
+    "revision": "0.8k1cjmndtog"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
