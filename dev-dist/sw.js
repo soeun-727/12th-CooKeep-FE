42ce3906bcd465b2587569192760216095c3203c
ji070 <jiye2705@gmail.com>
2026-02-12 13:56:50 +0900

    feat: 채택 페이지 경로 이동 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.ff2j626gako"
+    "revision": "0.7nm0ghpamg8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
