10fc43faeb6e7d75c7e38a681c845d25c3f2fba3
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-02-13 15:13:43 +0900

    fix: 쿠킵스 페이지 스크롤 영역 문제 및 레이아웃 수정

--- src/pages/cookeeps/CookeepsPage.tsx
+++ src/pages/cookeeps/CookeepsPage.tsx
@@ -193,7 +193,7 @@ export default function CookeepsPage() {
   };
 
   return (
-    <div className="h-[100dvh] flex flex-col overflow-hidden relative">
+    <div className="flex-1 flex flex-col min-h-0 relative no-scrollbar">
       {/* 1. 온보딩 */}
       <OnboardingModal
         isOpen={derivedModal === "onboarding"}
@@ -302,7 +302,7 @@ export default function CookeepsPage() {
         <CookeepsHeader />
       </div>
 
-      <div className="px-4 pt-4 shrink-0">
+      <div className="px-4 shrink-0">
         <PlantGrowthCard
           plant={currentPlant?.plantName}
           onWaterSuccess={handleWaterSuccess}
@@ -317,7 +317,7 @@ export default function CookeepsPage() {
       </div>
 
       {/* ===== 스크롤 영역 ===== */}
-      <div className="flex-1 overflow-y-auto no-scrollbar px-4 space-y-6 pt-5 pb-6">
+      <div className="flex-1 overflow-y-auto no-scrollbar px-4 space-y-[10px] pt-5 pb-6">
         <WeeklyTop3Section users={top3Users} />
         <WeeklyRecipeSection topRecipes={topRecipes} />
       </div>
