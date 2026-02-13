487f7e88b994e607e8b63dc54ff5db091940b029
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-02-13 20:32:56 +0900

    fix: 모달 문구 변경

--- src/components/myCookeep/record/UploadCompleteModal.tsx
+++ src/components/myCookeep/record/UploadCompleteModal.tsx
@@ -47,9 +47,12 @@ export default function UploadCompleteModal({
             alt="congrats"
           />
 
-          <p className="typo-body1 mb-6 text-center font-bold text-neutral-900">
+          <p className="typo-body1 text-center font-bold text-neutral-900">
             오늘의 레시피 등록 완료!
           </p>
+          <span className="typo-body text-(--color-green-deep) !font-bold mb-4">
+            쿠키 +1 🍪
+          </span>
 
           <Button
             size="S"
