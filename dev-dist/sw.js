46ec3ca23cffa4270ba153941f4835c6a3d5523c
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-02-14 02:14:20 +0900

    fix: 초기 진입화면 ui 버그 해결

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.se5m58c2d98"
+    "revision": "0.28r7c9mcppo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
