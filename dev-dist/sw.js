8ebea20a2ef6075b281444afa0173981716073f9
ji070 <jiye2705@gmail.com>
2026-02-15 23:26:04 +0900

    sw.js 파일문제로 다시 올리기

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.o3nibf3pico"
+    "revision": "0.uaatod07vh"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
