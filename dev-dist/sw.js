4248a9085ae06143a866bf1d8a4caf4b125f7053
ji070 <jiye2705@gmail.com>
2026-02-15 16:18:34 +0900

    feat: 이용약관전무 다시 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.2me4oo867cg"
+    "revision": "0.ssc07leqp6g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
