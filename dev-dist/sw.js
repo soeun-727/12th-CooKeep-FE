57a72cfa2d6596a67e185d5933da3f720d1454ec
ji070 <jiye2705@gmail.com>
2026-02-16 00:21:10 +0900

    fix: 레시피탭 선택 페이지 간격 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.rhh2lmqo93"
+    "revision": "0.1oqcqgkh9t8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
