3082012377c6d4c6064d171955fd56e7b3edb8d9
ji070 <jiye2705@gmail.com>
2026-02-18 15:32:56 +0900

    fix: 태그 부분 간격 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.jemot75ufh8"
+    "revision": "0.3ean9hun5fo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
