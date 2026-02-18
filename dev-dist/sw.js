1179dac6db69d88cfd0e83b0c4a3bc525431f562
ji070 <jiye2705@gmail.com>
2026-02-18 16:57:58 +0900

    fix: 레시피 과정 부분 인덱스 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.3ean9hun5fo"
+    "revision": "0.99pfcc9m98o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
