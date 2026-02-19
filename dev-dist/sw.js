3c44c7e8663a92d5a42c86033eb90dd1c472edd8
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-02-20 02:27:45 +0900

    fix: 서서히 사라지도록

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.d2nvvgqd0m"
+    "revision": "0.qmifv7pgup"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
