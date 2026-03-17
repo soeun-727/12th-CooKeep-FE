74917cde3c4aa1e37ca52a87aca2a42feef4a43a
ji070 <jiye2705@gmail.com>
2026-03-17 21:02:39 +0900

    fix: 비회원조회 레이아웃수정

--- src/pages/auth/GuestPage.tsx
+++ src/pages/auth/GuestPage.tsx
@@ -22,7 +22,7 @@ export default function GuestPage() {
   };
 
   return (
-    <div className="relative w-screen h-dvh bg-[#474747] overflow-hidden">
+    <div className="relative w-full h-dvh bg-[#474747] overflow-hidden">
       <div
         className="absolute inset-0 flex items-center justify-center"
         onClick={handleNext}
