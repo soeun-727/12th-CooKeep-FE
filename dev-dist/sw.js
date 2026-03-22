b5a67491dbf317c7b4b05a925f3911e0a417e459
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-03-22 17:56:53 +0900

    fix: 상태영역 오류 해결 시도

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.0re9bmk7ddo"
+    "revision": "0.54cnru4i2d8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
