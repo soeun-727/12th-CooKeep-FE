8ad0810a5e4857ceff9122bb2eb24924ed22e928
ji070 <jiye2705@gmail.com>
2026-03-31 21:33:58 +0900

    feat: 사진 등록 쿠키 모달(백엔드 요청사항 제외하고 완료)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.ri9qcq5q05"
+    "revision": "0.5gaan62f4f"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
